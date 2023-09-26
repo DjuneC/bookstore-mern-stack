03a3f58d62f96442a72b0fccb107b9deffded626
DjuneC <cesarrodelin@outlook.com>
2023-09-26 11:24:51 -0500

    Add (create & get) for Book model

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,12 @@ dotenv.config();
 import express from "express";
 import { PORT, connectDB } from "./config.js";
 import mongoose from "mongoose";
+import Book from "./models/Book.js"
 
 const app = express();
+
+//middlewares
+app.use(express.json());
 
 //if db connection successful listen to a port
 mongoose
@@ -20,3 +24,32 @@ mongoose
   .catch((error) => {
     console.log(error);
   });
+
+app.get('/', (req, res) => {
+    return res.status(200).json({"message":"Everything went through"})
+});
+
+app.post('/create-book', async (req, res) => {
+  const {title, author, publishYear} = req.body;
+  
+  try {
+    const book = new Book (
+      {
+        title, author, publishYear
+      }
+    )
+    await book.save();
+    return res.status(201).json(book._id);
+  } catch (error) {
+    return res.status(500).json({'message': 'Failed to create the new book'})
+  }
+});
+
+app.get('/get-all-books', async (req, res) => {
+  try {
+    const books = await Book.find({}).populate('author');
+    return res.status(200).json(books);
+  } catch (error) {
+    return res.status(500).json({"message" : "Failed to fetch all books"});
+  }
+});
