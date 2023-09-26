a6d9381212e9e57f8ffae769d39d1b874d855b19
DjuneC <cesarrodelin@outlook.com>
2023-09-26 09:12:41 -0500

    Modify format document

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,18 @@
 import express from "express";
-import {PORT, connectDB} from "./config.js"
+import { PORT, connectDB } from "./config.js";
 import mongoose from "mongoose";
 
 const app = express();
 
 //if db connection successful listen to a port
 mongoose
-.connect(connectDB)
-.then(()=>{
+  .connect(connectDB)
+  .then(() => {
     console.log("App connected to database");
     app.listen(PORT, () => {
-        console.log("Server is up and running at port : " + PORT)
+      console.log("Server is up and running at port : " + PORT);
     });
-})
-.catch((error)=>{
+  })
+  .catch((error) => {
     console.log(error);
-})
+  });
