caff91eda96aae55d03c8d50fa946c0c82bb22cd
DjuneC <cesarrodelin@outlook.com>
2023-09-27 10:53:25 -0500

    Create logic to handle book registration

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -1,7 +1,47 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Form from "../components/Form";
+
 const CreateBook = () => {
+  const [book, setBook] = useState({
+    title: "",
+    author: "",
+    publishYear: ""
+  })
+  const [loading, setLoading] = useState(false)
+  const navigate = useNavigate();
+
+  const handleSaveBook = async (e) => {
+    e.preventDefault();
+    const data = {
+      title: book.title,
+      author: book.author,
+      publishYear: book.publishYear
+    }
+    setLoading(true);
+    await axios
+    .post("http://127.0.0.1:5555/api/v1/book/create", data)
+    .then(() => {
+      navigate('/');
+    })
+    .catch((error) => {
+      console.log(error);
+      alert("An error has been throw while registering this book")
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }
   return (
-    <div>CreateBook</div>
-  )
+    <Form
+      typeForm="Create"
+      book={book}
+      setBook={setBook}
+      loading={loading}
+      handleSubmit={handleSaveBook}
+    />
+  );
 }
 
 export default CreateBook
