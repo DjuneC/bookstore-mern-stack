06e291987ea7d139d73e51fd9d4d16b3338a8e6e
DjuneC <cesarrodelin@outlook.com>
2023-09-27 07:20:47 -0500

    Set up the router for the main menu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,19 @@
+import ShowBook from "./pages/AllBook";
+import CreateBook from "./pages/CreateBook";
+import Home from "./pages/Home";
+import DeleteBook from "./pages/DeleteBook";
+import UpdateBook from "./pages/UpdateBook";
+import {Routes, Route} from "react-router-dom";
+
 const App = () => {
   return (
-    <div className='bg-red-500 text-white'>App</div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/books/create' element={<CreateBook/>} />
+      <Route path='/books/show/:id' element={<ShowBook/>} />
+      <Route path='/books/remove/:id' element={<DeleteBook/>} />
+      <Route path='/books/update/:id' element={<UpdateBook/>} />
+    </Routes>
   )
 }
 
